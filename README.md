53c7ed3f8d0ec5635ac49f33459b2d1e4001b993
Tony Li <litony1999@gmail.com>
2024-02-14 14:19:34 -0800

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-Simple app allows users to draw mazes on the grid and watch how the differenct algorithems find the shortest path!
-
-1. use mouse to draw obstacles
-2. select your desired algo
-3. watch as it computes shortest path!
-
-Full app at https://tonyli29.github.io/algoVisualizer/
---
+++ precache-manifest.5bcb151562775280934bbaef1a1bcb5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1572116c71f0649c4f497009e3811aa8",
+    "url": "/algoVisualizer/index.html"
+  },
+  {
+    "revision": "e5a311a718efb04c6fb0",
+    "url": "/algoVisualizer/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "eac2ae7f103cfde429b5",
+    "url": "/algoVisualizer/static/css/main.1ca6b690.chunk.css"
+  },
+  {
+    "revision": "e5a311a718efb04c6fb0",
+    "url": "/algoVisualizer/static/js/2.ad0178e3.chunk.js"
+  },
+  {
+    "revision": "eac2ae7f103cfde429b5",
+    "url": "/algoVisualizer/static/js/main.d4abfa8c.chunk.js"
+  },
+  {
+    "revision": "04ca22b2a29877742bda",
+    "url": "/algoVisualizer/static/js/runtime-main.15bff50d.js"
+  }
+]);
